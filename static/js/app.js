40ba99da819eae0f216b2b441e7e699fa79cde58
Adam <70108536+fanchea84@users.noreply.github.com>
2021-01-07 15:49:33 -0800

    javascript challenge

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+// base template sourced from bootcamp starter files, then I added some stuff to make it awesome
+// https://oregon.bootcampcontent.com/Oregon_Coding_Bootcamp/uofo-por-data-pt-09-2020-u-c/-/blob/master/14-Intro-To-JavaScript/HW/Instructions/StarterCode/static/js/app.js
+
+var tableData = data;
+
+// YOUR CODE HERE!
